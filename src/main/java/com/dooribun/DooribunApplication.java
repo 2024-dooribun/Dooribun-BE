37d43b2d289fc64dfc4a514a15d0f1cfc1406587
eeezi <alloy0035@hanmail.net>
2024-01-29 16:05:06 +0900

    Project Initialization

---
+++ src/main/java/com/dooribun/DooribunApplication.java
@@ -0,0 +1,13 @@
+package com.dooribun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DooribunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DooribunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dooribun/DooribunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dooribun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DooribunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
